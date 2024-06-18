c3914554044d3eb918b11d692469dada549b3d0d
Fiodor <fiodor.m2016@yandex.ru>
2024-06-18 22:19:08 +0300

    показывает персональную инфу в Тиндере

--- OOP_BIG_PROJECT/Controllers/MatchController.cs
+++ OOP_BIG_PROJECT/Controllers/MatchController.cs
@@ -1,81 +1,91 @@
-﻿using Azure;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using OOP_BIG_PROJECT.Data;
 using OOP_BIG_PROJECT.Models;
 using OOP_BIG_PROJECT.ViewModels;
-using System.Security.Claims;
-using OOP_BIG_PROJECT.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OOP_BIG_PROJECT.Controllers
 {
     public class MatchController : Controller
-	{
-		private readonly ApplicationDbContext _context;
+    {
+        private readonly ApplicationDbContext _context;
 
-		public MatchController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
-
-        // Метод для создания матча
-        //public async Task<IActionResult> Create(int userId, int gameId, int venueId, DateTime scheduledTime)
-        //{
-        //	var match = new Match
-        //	{
-        //		//UserId1 = User.FindFirstValue(ClaimTypes.NameIdentifier),
-        //		UserId2 = userId,
-        //		GameId = gameId,
-        //		PlaceId = venueId,
-        //		ScheduledTime = scheduledTime,
-        //		IsAccepted = false
-        //	};
-
-        //	_context.Matches.Add(match);
-        //	await _context.SaveChangesAsync();
-
-        //	return RedirectToAction("Index", "Home");
-        //}
-
-        //// Метод для подтверждения матча
-        //[HttpPost]
-        //public async Task<IActionResult> Accept(int matchId)
-        //{
-        //	var match = await _context.Matches.FindAsync(matchId);
-        //	match.IsAccepted = true;
-        //	await _context.SaveChangesAsync();
-
-        //	return RedirectToAction("Index", "Home");
-        //}
-        [HttpGet]
-        public IActionResult Index()
+        public MatchController(ApplicationDbContext context)
         {
-            var response = new FighterViewModel();
-            return View(response);
+            _context = context;
         }
         [HttpPost]
         public IActionResult Index(FighterViewModel A)
         {
-            List<Fighter> Fighters=_context.Fighter.Where(a=>a.Id!=StaticStuff.Fighter.Id).ToList();
+            Fighter selectedFighter = GetRandomFighter();
+            A.SelectedFighter = selectedFighter;
+
+            _context.SaveChanges(); // Сохранение изменений в базу данных
+
+            return RedirectToAction("Index"); // Перенаправление на GET-версию метода Index
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Fighter selectedFighter = GetRandomFighter();
+            var response = new FighterViewModel
+            {
+                SelectedFighter = selectedFighter
+            };
+
+            return View(response);
+        }
+        private Fighter GetRandomFighter()
+        {
+            List<Fighter> Fighters = _context.Fighter.Where(a => a.Id != StaticStuff.Fighter.Id).ToList();
             Random random = new Random();
+
             if (Fighters.Count == 0)
             {
                 throw new InvalidOperationException("Массив пуст.");
             }
-            int index = random.Next(Fighters.Count);
 
+            int index = random.Next(Fighters.Count);
             Fighter selectedFighter = Fighters[index];
-            A.SelectedFighter=selectedFighter;
             Fighters.RemoveAt(index);
-            return View(A);
-            _context.SaveChanges();
-            RedirectToAction("Index");
+
+            return selectedFighter;
         }
 
-        //[HttpGet]
-        //public IActionResult Index()
-        //{
-        //    var response = new FighterViewModel();
-        //    return View(response);
-        //}
     }
+
 }
+
+
+// Метод для создания матча
+//public async Task<IActionResult> Create(int userId, int gameId, int venueId, DateTime scheduledTime)
+//{
+//	var match = new Match
+//	{
+//		//UserId1 = User.FindFirstValue(ClaimTypes.NameIdentifier),
+//		UserId2 = userId,
+//		GameId = gameId,
+//		PlaceId = venueId,
+//		ScheduledTime = scheduledTime,
+//		IsAccepted = false
+//	};
+
+//	_context.Matches.Add(match);
+//	await _context.SaveChangesAsync();
+
+//	return RedirectToAction("Index", "Home");
+//}
+
+//// Метод для подтверждения матча
+//[HttpPost]
+//public async Task<IActionResult> Accept(int matchId)
+//{
+//	var match = await _context.Matches.FindAsync(matchId);
+//	match.IsAccepted = true;
+//	await _context.SaveChangesAsync();
+
+//	return RedirectToAction("Index", "Home");
+//}
