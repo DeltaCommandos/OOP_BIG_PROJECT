35bb27ae7335dc62836182a5dfb19c250465ba06
Inoet <lenin110@inbox.ru>
2024-06-18 22:23:46 +0300

    Работа с лайками

---
+++ OOP_BIG_PROJECT/Models/Likes.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+using OOP_BIG_PROJECT.ViewModels;
+
+namespace OOP_BIG_PROJECT.Models
+{
+    public class Likes
+    {
+        public int Id1 { get; set; }
+        public int Id2 { get; set; }
+        public bool Like1 { get; set; }
+        public bool Like2 { get; set; }
+    }
+}
