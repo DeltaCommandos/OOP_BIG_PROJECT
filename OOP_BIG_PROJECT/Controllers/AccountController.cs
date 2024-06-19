61c8bfd923a3c9d0004ff8fa5975d476a246ac6b
Fiodor <fiodor.m2016@yandex.ru>
2024-06-19 12:52:42 +0300

    Сделал Таблицу Match .метод ViewMatches. Account

--- OOP_BIG_PROJECT/Controllers/AccountController.cs
+++ OOP_BIG_PROJECT/Controllers/AccountController.cs
@@ -5,6 +5,8 @@ using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
 using OOP_BIG_PROJECT.ViewModels;
 using OOP_BIG_PROJECT.Data;
+using System.Collections.Generic;
+using System.Linq;
 namespace OOP_BIG_PROJECT.Controllers
 {
     //кнопку "начать поиск". аву в углу сделать. рядом с ней изменить фото. сделать изменить увлечения. меню с предстоящими боями. 
@@ -29,9 +31,27 @@ namespace OOP_BIG_PROJECT.Controllers
             return View(response);
         }
         [HttpGet]
-        public IActionResult Accaunt()
+        public IActionResult Account()
         {
             var response = new FighterViewModel();
+            return View(response);
+        }
+        [HttpGet]
+        public IActionResult AccountHome()
+        {
+            var response = new UserViewModel();
+            return View(response);
+        }
+        //[HttpGet]
+        //public IActionResult ViewMatches()
+        //{
+        //    var response = new FighterViewModel();
+        //    return View(response);
+        //}
+        [HttpGet]
+        public IActionResult ChangeLoginAndPassword()
+        {
+            var response = new UserViewModel();
             return View(response);
         }
         [HttpPost]
@@ -47,6 +67,52 @@ namespace OOP_BIG_PROJECT.Controllers
         public IActionResult Account(FighterViewModel A)
         {
             return RedirectToAction("Account", "AccountHome");
+        }
+        [HttpGet]
+        public IActionResult ViewMatches(FighterViewModel A)
+        {
+            int currentFighterId = StaticStuff.Fighter.Id;
+
+            // Получаем список всех бойцов, кроме текущего
+            List<Fighter> fighters = _context.Fighter.Where(a => a.Id != currentFighterId).ToList();
+
+            // Получаем список идентификаторов бойцов, которые понравились текущему бойцу
+            List<int> likedFighterIds = _context.Likes
+                                              .Where(l => l.LikerId == currentFighterId)
+                                              .Select(l => l.LikedFighterId)
+                                              .ToList();
+
+            // Сохраняем список идентификаторов в модель для использования в представлении
+            A.likedFighterIds = likedFighterIds;
+
+            // Создаем список для хранения пар бойцов, которые лайкнули друг друга
+            List<Tuple<Fighter, Fighter>> mutualLikes = new List<Tuple<Fighter, Fighter>>();
+
+            foreach (int likedFighterId in likedFighterIds)
+            {
+                // Проверяем, лайкнул ли текущий боец бойца с идентификатором likedFighterId
+                bool isMutualLike = _context.Likes
+                                          .Any(l => l.LikerId == likedFighterId && l.LikedFighterId == currentFighterId);
+
+                if (isMutualLike)
+                {
+                    // Находим объекты бойцов, которые лайкнули друг друга
+                    Fighter currentFighter = fighters.FirstOrDefault(f => f.Id == likedFighterId);
+                    Fighter likedFighter = _context.Fighter.FirstOrDefault(f => f.Id == likedFighterId);
+
+                    // Добавляем пару бойцов в список mutualLikes
+                    mutualLikes.Add(new Tuple<Fighter, Fighter>(currentFighter, likedFighter));
+                }
+            }
+
+            // Сохраняем список пар взаимных лайков в модель для использования в представлении
+            A.MutualLikes = mutualLikes;
+            return View(A);
+        }
+        [HttpPost]
+        public IActionResult ChangeLoginAndPassword(UserViewModel A)
+        {
+            return View(A);
         }
 
     }
--- OOP_BIG_PROJECT/ViewModels/FighterViewModel.cs
+++ OOP_BIG_PROJECT/ViewModels/FighterViewModel.cs
@@ -8,6 +8,8 @@ namespace OOP_BIG_PROJECT.ViewModels
         public int FighterId { get; set; }
 
         public int Mode { get; set; }
+        public List<int> likedFighterIds { get; set; }
         public Fighter SelectedFighter { get; set; }
+        public List<Tuple<Fighter, Fighter>> MutualLikes { get; set; }
     }
 }
