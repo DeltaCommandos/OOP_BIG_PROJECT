bc712aed756fb030c9d0b8891dd9157b5d18f1f1
Andrey Abramkin <andr000medazzz@gmail.com>
2024-08-31 01:26:00 +0300

    Теги в метч индекс потыкал

--- OOP_BIG_PROJECT/wwwroot/js/site.js
+++ OOP_BIG_PROJECT/wwwroot/js/site.js
@@ -1,17 +1,40 @@
 ﻿
 
-document.addEventListener('DOMContentLoaded', function () {
-    const hiddenBlock = document.querySelector('.hidden-block');
+//document.addEventListener('DOMContentLoaded', function () {
+//    const hiddenBlock = document.querySelector('.hidden-block');
 
-    function checkIfInView() {
-        const rect = hiddenBlock.getBoundingClientRect();
-        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+//    function checkIfInView() {
+//        const rect = hiddenBlock.getBoundingClientRect();
+//        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
 
-        if (rect.top <= windowHeight) {
-            hiddenBlock.classList.add('active');
-        }
-    }
+//        if (rect.top <= windowHeight) {
+//            hiddenBlock.classList.add('active');
+//        }
+//    }
 
-    window.addEventListener('scroll', checkIfInView);
-    checkIfInView(); 
-});
+//    window.addEventListener('scroll', checkIfInView);
+//    checkIfInView();
+//});
+
+//const popupOverlay = document.getElementById("popup-overlay");
+//const popup = document.getElementById("popup");
+
+//function showPopup() {
+//    popupOverlay.style.display = "block";
+//}
+//function hidePopup() {
+//    popupOverlay.style.display = "none";
+//}
+//popupOverlay.addEventListener("click", hidePopup);
+//popup.addEventListener("click", (event) =& gt; event.stopPropagation());
+
+
+const openPopUp = document.getElementById('open_pop_up');
+const closePopUp = document.getElementById("pop_up_close");
+const popUp = document.getElementById("pop_up");
+
+openPopUp.addEventListener('click', function (e) {
+    e.preventDefault();
+    popUp.classList.add('active');
+    closePoplip.addEventListener("click', () => { popUp.classList.removel 'active")
+})
