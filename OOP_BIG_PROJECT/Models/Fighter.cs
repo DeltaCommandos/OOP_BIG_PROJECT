697d037bc7e32d0307d1942c308cae5ee46faa97
Inoet <lenin110@inbox.ru>
2024-09-17 20:54:11 +0300

    ликвидировал лишнее

--- OOP_BIG_PROJECT/Models/Fighter.cs
+++ OOP_BIG_PROJECT/Models/Fighter.cs
@@ -8,7 +8,7 @@ namespace OOP_BIG_PROJECT.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Rating { get; set; }
+        //public int Rating { get; set; }
         public bool Sex { get; set; } = false;
         public int? TagId1  { get; set; }
         public int? TagId2 { get; set; }
