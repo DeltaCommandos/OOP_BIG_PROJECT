e1c6641f71f376d6d4a796a07c9650c7d4a18949
Inoet <lenin110@inbox.ru>
2024-09-18 17:27:31 +0300

    Переходы переходятся

--- OOP_BIG_PROJECT/Controllers/HomeController.cs
+++ OOP_BIG_PROJECT/Controllers/HomeController.cs
@@ -212,7 +212,7 @@ namespace OOP_BIG_PROJECT.Controllers
                         _context.SaveChanges();
                         StaticStuff.Admin = _context.Admin.FirstOrDefault(a => a.UserId == User.Id);
                         //Добавить переход на аккаунт админа
-                        return RedirectToAction("Admin");
+                        return RedirectToAction("Index", "Admin");
                     }
                 }
             }
